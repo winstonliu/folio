5524954968222aaf483056e42450f379459765fd
Winston Liu <winston.liu@mail.utoronto.ca>
2019-06-01 11:18:50 -0700

    Adding icon file

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QFileSystemModel>
+#include <QResource>
 #include <QTreeView>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    Q_INIT_RESOURCE(resources);
+
+    QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/default.png"));
+
     // Temporary until I get around to adding a config file
     Folio folio("C:/Users/Winston Liu/Dropbox/Writing");
-
     // Path is temporary until I add a config file
     folio.setCommand("C:/Program Files (x86)/Vim/vim80/gvim.exe");
-
     folio.show();
 
-    return a.exec();
+    return app.exec();
 }
